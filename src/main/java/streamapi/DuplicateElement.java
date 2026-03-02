52d51a0b537bdd61bf02f19202d9cc4be8eb2e3b
jeyasree123 <jeyasreechandran123@gmail.com>
2026-03-02 23:21:47 +0800

    updated

--- src/main/java/streamapi/DuplicateElement.java
+++ src/main/java/streamapi/DuplicateElement.java
@@ -2,10 +2,19 @@ package streamapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DuplicateElement {
     public static void main(String[] args) {
         List<Integer> list= Arrays.asList(10,15,8,49,25,98,98,32,15);
-        list.stream().distinct().forEach(System.out::println);
+        Map<Integer,Long> duplicateElements=list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        duplicateElements.forEach((Key,Value)->{
+            if(Value>1){
+                System.out.println(Key);
+            }
+
+        });
     }
 }
